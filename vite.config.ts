44dbfea1a05f396147559c7f815e97cc18bcfe0d
Mishita Gogia <gogiamishita@gmail.com>
2026-06-12 01:21:16 +0530

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig(() => {
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+      allowedHosts: ['voicecv-ai-resume.onrender.com', 'voicecvai-forked.onrender.com','localhost'],  // ✅ add this
+    },
+  };
+});
