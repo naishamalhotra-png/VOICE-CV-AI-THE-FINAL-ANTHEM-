e79386a04fffba4d8918d6506cdb281297bbdf8c
lolrawr <naishamalhotra070906@gmail.com>
2026-06-12 17:03:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,20 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
+
   server: {
     allowedHosts: ['voice-cv-ai-the-final-anthem.onrender.com'],
   },
+
   preview: {
     allowedHosts: ['voice-cv-ai-the-final-anthem.onrender.com'],
   },
-})
+});
