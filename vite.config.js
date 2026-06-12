59f435bf3b147ba3b3a36f5f19edf31f316cb819
lolrawr <naishamalhotra070906@gmail.com>
2026-06-12 16:58:39 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig(() => {
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
 
 export default defineConfig({
   plugins: [react()],
